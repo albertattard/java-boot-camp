77b27fc7f1f764f5c3dec9554a232babd9ed49e1
Albert Attard <albert.attard@thoughtworks.com>
2020-06-12 13:26:36 +0200

    Start working on the copy-code

---
+++ assets/js/copy-code.js
@@ -0,0 +1,23 @@
+const copyCode = (clickEvent) => {
+    console.log("clicked...")
+    // const copyCodeButton = clickEvent.target;
+    // const tempTextArea = document.createElement('textarea');
+    // tempTextArea.textContent = copyCodeButton.getAttribute('data-code');
+    // document.body.appendChild(tempTextArea);
+    //
+    // const selection = document.getSelection();
+    // selection.removeAllRanges();
+    // tempTextArea.select();
+    // document.execCommand('copy');
+    // selection.removeAllRanges();
+    // document.body.removeChild(tempTextArea);
+    //
+    // copyCodeButton.classList.add('copied');
+    // setTimeout(() => {
+    //     copyCodeButton.classList.remove('copied');
+    // }, 2000);
+};
+
+// document.querySelectorAll('.copy-code-button').forEach((copyCodeButton) => {
+//     copyCodeButton.addEventListener('click', copyCode);
+// });
