37866a6172e375721da39ab6aa0c82289b1e34ff
paul-boerding <pboerdin@thoughtworks.com>
2020-06-12 13:53:50 +0200

    Fix the copy-to-clipboard button [PB]

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,23 +1,17 @@
-const copyCode = (clickEvent) => {
-    console.log("clicked...")
-    // const copyCodeButton = clickEvent.target;
-    // const tempTextArea = document.createElement('textarea');
-    // tempTextArea.textContent = copyCodeButton.getAttribute('data-code');
-    // document.body.appendChild(tempTextArea);
-    //
-    // const selection = document.getSelection();
-    // selection.removeAllRanges();
-    // tempTextArea.select();
-    // document.execCommand('copy');
-    // selection.removeAllRanges();
-    // document.body.removeChild(tempTextArea);
-    //
-    // copyCodeButton.classList.add('copied');
-    // setTimeout(() => {
-    //     copyCodeButton.classList.remove('copied');
-    // }, 2000);
-};
+const copyCode = (copyCodeButton) => {
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.textContent = copyCodeButton.getAttribute('data-code');
+    document.body.appendChild(tempTextArea);
 
-// document.querySelectorAll('.copy-code-button').forEach((copyCodeButton) => {
-//     copyCodeButton.addEventListener('click', copyCode);
-// });
+    const selection = document.getSelection();
+    selection.removeAllRanges();
+    tempTextArea.select();
+    document.execCommand('copy');
+    selection.removeAllRanges();
+    document.body.removeChild(tempTextArea);
+
+    copyCodeButton.classList.add('copied');
+    setTimeout(() => {
+        copyCodeButton.classList.remove('copied');
+    }, 2000);
+};
